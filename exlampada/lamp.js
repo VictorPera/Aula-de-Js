abeb64022034f72bdf1f93f4484a5eec77bcc626
VictorPera <victorhugo.pera@gmail.com>
2023-08-03 22:22:51 -0300

    interruptor alterado

--- exlampada/lamp.js
+++ exlampada/lamp.js
@@ -1,13 +1,22 @@
 function  ligaDesliga(){
+    console.log("chamou")
     let lampada = document.getElementById("lampada");
-    let interruptor = document.getElementById("btnPrincipal");
+    let interruptor = document.getElementById("interruptor_desligado")
+    // let interruptor = document.getElementById("btnPrincipal");
 
     if(lampada.src.match("/exlampada/img/lampadaApagada.jpeg")){
         lampada.src = "/exlampada/img/lampadaAcessa.jpeg";
-        interruptor.innerText = 'Desligar';
+        // interruptor.innerText = 'Desligar';
+        interruptor.src = "/exlampada/img/interruptor_desligado.jpeg";
 
     }else{
         lampada.src = "/exlampada/img/lampadaApagada.jpeg";
-        interruptor.innerText = 'Ligar';
+        // interruptor.innerText = 'Ligar';
+        interruptor.src = "/exlampada/img/interruptor_ligado.jpeg";
     }
 }
+
+let interruptor = document.getElementById("interruptor_desligado");
+console.log(interruptor)
+interruptor.addEventListener("click", ligaDesliga);
+
