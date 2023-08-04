8d423333244b686756855bff1cb49bac5a41dc38
VictorPera <victorhugo.pera@gmail.com>
2023-08-03 21:54:42 -0300

    Exercicíos feitos

---
+++ ex de funcionalidades/ex1.js
@@ -0,0 +1,6 @@
+window.alert("teste");
+
+function mudarTexto(){
+    let tituloElemento = document.getElementById("titulo");
+    tituloElemento.innerHTML = "Titulo Novo";
+}
---
+++ exlampada/lamp.js
@@ -0,0 +1,18 @@
+function  ligaDesliga(){
+    let lampada = document.getElementById("lampada");
+    let interruptor = document.getElementById("btnPrincipal");
+
+    if(lampada.src.match("/exlampada/img/lampadaApagada.jpeg")){
+        lampada.src = "/exlampada/img/lampadaAcessa.jpeg";
+        interruptor.innerText = 'Desligar';
+
+    }else{
+        lampada.src = "/exlampada/img/lampadaApagada.jpeg";
+        interruptor.innerText = 'Ligar';
+
+    }
+
+
+    
+    
+}
