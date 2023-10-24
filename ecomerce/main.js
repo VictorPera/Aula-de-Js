459cca8fe55937aedd42ae408b462402713fb573
VictorPera <victorhugo.pera@gmail.com>
2023-10-24 19:50:45 -0300

    a

--- ecomerce/main.js
+++ ecomerce/main.js
@@ -154,11 +154,11 @@ function renderizarCatalogoLivros(){
     for(const produtoCatalogo of catalogoBox){
 
         const cartaoProduto = `
-        <div id="card-produto-${produtoCatalogo.id}" class="flex flex-col w-80 m-2 p-2 justify-between shadow-xl shadow-slate-400 rounded-lg produto group ${produtoCatalogo.adolescente ? 'adolescente' : 'adulto'}">
+        <div id="card-produto-${produtoCatalogo.id}" class="flex flex-col w-[350px] m-2 p-2 justify-between shadow-xl shadow-slate-400 rounded-lg produto group ${produtoCatalogo.adolescente ? 'adolescente' : 'adulto'}">
             <img 
             src="./img/${produtoCatalogo.imagem}"
             alt="${produtoCatalogo.nome}"
-            class="drop-shadow-md group-hover:scale-110 duration-300 my-3 w-[290px] self-center" 
+            class="drop-shadow-md group-hover:scale-110 duration-300 my-3 w-[290px] h-[290px] self-center" 
             >
             <h2 class="text-sm">${produtoCatalogo.nome}</h2>
             <p class="text-lg">R&#x24; ${produtoCatalogo.preco}</p>
@@ -177,11 +177,11 @@ function renderizarCatalogoDiversos(){
     for(const produtoCatalogo of catalogoDiversos){
 
         const cartaoProduto = `
-        <div id="card-produto-${produtoCatalogo.id}" class="flex flex-col w-80 m-2 p-2 justify-between shadow-xl shadow-slate-400 rounded-lg produto group ${produtoCatalogo.adolescente ? 'adolescente' : 'adulto'}">
+        <div id="card-produto-${produtoCatalogo.id}" class="flex flex-col w-[350px] m-2 p-2 justify-between shadow-xl shadow-slate-400 rounded-lg produto group ${produtoCatalogo.adolescente ? 'adolescente' : 'adulto'}">
             <img 
             src="./img/${produtoCatalogo.imagem}"
             alt="${produtoCatalogo.nome}"
-            class="drop-shadow-md group-hover:scale-110 duration-300 my-3 w-[290px] self-center" 
+            class="drop-shadow-md group-hover:scale-110 duration-300 my-3 w-[290px] h-[290px] self-center" 
             >
             <h2 class="text-sm">${produtoCatalogo.nome}</h2>
             <p class="text-lg">R&#x24; ${produtoCatalogo.preco}</p>
